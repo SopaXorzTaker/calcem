e0394340323185bf38cccc4cc23f3f3e267e0607
LBPHacker <lbphacker@gmail.com>
2017-04-01 22:17:18 +0200

    Remove stray test code

--- src/test/nxu8test.c
+++ src/test/nxu8test.c
@@ -40,12 +40,9 @@ int main(int argc, char *argv[])
     }
     
     nxu8_cpu_reset(&context.cpu_state);
-    int lol = 0;
     while (context.running)
     {
         nxu8_cpu_next(&context.cpu_state);
-        ++lol;
-        if (lol == 1000) context.running = 0;
     }
     
     context_close(&context);
